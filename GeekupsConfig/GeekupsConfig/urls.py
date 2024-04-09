57c32f7b91e894c1a0a2512fcfd5735d9dfaa559
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-04-09 14:23:07 +0330

    start

--- GeekupsConfig/GeekupsConfig/urls.py
+++ GeekupsConfig/GeekupsConfig/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for GeeekupsConfig project.
+URL configuration for GeekupsConfig project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- GeekupsConfig/manage.py
+++ GeekupsConfig/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GeeekupsConfig.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GeekupsConfig.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
